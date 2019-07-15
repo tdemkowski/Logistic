d8aa54cfb08aaa55c3283d28544a137c1ff8663b
tdemkowski <tdemkowski96@gmail.com>
2019-07-15 19:29:43 +1000

    Adding new Worker types

---
+++ src/Workers/Courier.java
@@ -0,0 +1,16 @@
+package Workers;
+
+
+
+public class Courier implements Worker {
+
+    @Override
+    public int getSalary() {
+        return 0;
+    }
+
+    @Override
+    public int getHoursWorked() {
+        return 0;
+    }
+}
---
+++ src/Workers/Packer.java
@@ -0,0 +1,14 @@
+package Workers;
+
+public class Packer implements Worker {
+
+    @Override
+    public int getSalary() {
+        return 0;
+    }
+
+    @Override
+    public int getHoursWorked() {
+        return 0;
+    }
+}
